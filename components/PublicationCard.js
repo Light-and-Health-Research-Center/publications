4948eff03631e463fbfb5630a57ed86f0c91adab
vance <root.mvm@gmail.com>
2021-09-04 00:33:54 -0400

    card blue text -> black text

--- components/PublicationCard.js
+++ components/PublicationCard.js
@@ -11,27 +11,9 @@ export default function PublicationCard({ publication }) {
     >
       <div className="border border-black-20 rounded-lg p-4">
         <h4 className="font-bold leading-5 mb-2">{publication.title}</h4>
-        {publication.doi && (
-          <div className="block text-black-60">
-            <h6 className="inline-block mr-2">DOI:</h6>
-            {publication.doi}
-          </div>
-        )}
-        {publication.year && (
-          <div className="block text-black-60">
-            <h6 className="inline-block mr-2">Year:</h6>
-            {publication.year}
-          </div>
-        )}
-        {publication.journal && (
-          <div className="block text-black-60">
-            <h6 className="inline-block mr-2">Journal:</h6>
-            {`${publication.journal.name} ${publication.journal.extra}`}
-          </div>
-        )}
         {publication.authors && (
-          <div className="block text-black-60">
-            <h6 className="inline-block mr-2">Author(s):</h6>
+          <div className="block text-black-85">
+            <h6 className="inline-block mr-2 text-black-100">Author(s):</h6>
             {publication.authors.map((author, idx) => (
               <span key={author.last}>{`${author.last && author.last} ${
                 author.first && author.first[0]
@@ -41,9 +23,33 @@ export default function PublicationCard({ publication }) {
             ))}
           </div>
         )}
+        {publication.year && (
+          <div className="block text-black-85">
+            <h6 className="inline-block mr-2 text-black-100">Year:</h6>
+            {publication.year}
+          </div>
+        )}
+        {publication.fund_number && (
+          <div className="block text-black-85">
+            <h6 className="inline-block mr-2 text-black-100">Funding:</h6>
+            {publication.fund_number}
+          </div>
+        )}
+        {publication.journal && (
+          <div className="block text-black-85">
+            <h6 className="inline-block mr-2 text-black-100">Journal:</h6>
+            {`${publication.journal.name} ${publication.journal.extra}`}
+          </div>
+        )}
+        {publication.doi && (
+          <div className="block text-black-85">
+            <h6 className="inline-block mr-2 text-black-100">DOI:</h6>
+            {publication.doi}
+          </div>
+        )}
         {publication.keys && (
-          <div className="block text-black-60">
-            <h6 className="inline-block mr-2">Key(s):</h6>
+          <div className="block text-black-85">
+            <h6 className="inline-block mr-2 text-black-100">Key(s):</h6>
             {publication.keys.map((key, idx) => (
               <span key={key}>{`${key}${
                 idx === publication.keys.length - 1 ? "" : ", "
